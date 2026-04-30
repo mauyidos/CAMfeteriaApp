0a065a26e4d30d1976e71b6b74101988bfa708f0
mauyidos <mauyidos@gmail.com>
2026-04-30 15:20:50 -0600

    Se agregaron nuevas imagenes al menú

--- app/src/main/java/com/example/camfeteriaapp/data/ProductosData.kt
+++ app/src/main/java/com/example/camfeteriaapp/data/ProductosData.kt
@@ -6,8 +6,8 @@ import com.example.camfeteriaapp.R
 
 object ProductosData {
     val listaProductos = listOf(
-        Producto(1, "Café Americano", 25.0, R.drawable.cafe_americano),
-        Producto(2, "Capuccino", 40.0, R.drawable.cappuccino),
-        Producto(3, "Pan dulce", 20.0, R.drawable.concha)
+        Producto(1, "Café Americano", 25.0, R.drawable.pcafeamericano),
+        Producto(2, "Capuccino", 40.0, R.drawable.pcapuccino),
+        Producto(3, "Pan dulce", 20.0, R.drawable.pconcha)
     )
 }
--- app/src/main/java/com/example/camfeteriaapp/ui/components/ProductoCard.kt
+++ app/src/main/java/com/example/camfeteriaapp/ui/components/ProductoCard.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.camfeteriaapp.model.Producto
@@ -31,7 +34,10 @@ fun ProductoCard(
             Image(
                 painter = painterResource(id = producto.imagenResId),
                 contentDescription = producto.nombre,
-                modifier = Modifier.size(80.dp)
+                modifier = Modifier
+                    .size(115.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
 
             Column(
